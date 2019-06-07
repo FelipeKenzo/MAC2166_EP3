8bdfbce77de532fccb8c30abd42ccf4f649c2f55
FelipeKenzo <felipe.kenzo@live.com>
2019-06-07 20:53:17 -0300

    Opção 1

--- mac2166_ep3.py
+++ mac2166_ep3.py
@@ -1,11 +1,58 @@
-def opcao0(n, tabuleiro):
-    """Opcao 1"""
+def main():
+    opcao = int(input("Opcao de jogo: "))
+    n = int(input("Tamanho do tabuleiro: "))
 
-def opcao1():
-    """Opcao 2"""
+    if (opcao == 0):
+        opcao0(n)
+    elif (opcao == 1):
+        opcao1(n)
+    elif (opcao == 2):
+        opcao2(n)
+
+def opcao0(n):
+    """Opcao 0"""
     
-def opcao2():
+    print("Tabuleiro inicial:", end = " ")
+    tabInicial = tabuleiro(n)
+
+    print("Tabuleiro final:", end = " ")
+    tabFinal = tabuleiro(n)
+
+    iguais = True
+    for i in range(n):
+        for j in range(n):
+            if (tabInicial[i][j] != tabFinal[i][j]):
+                iguais = False
+
+    if (iguais):
+        print("SIM")
+    else:
+        print("NAO")
+
+def opcao1(n):
     """Opcao 1"""
+    
+def opcao2(n):
+    """Opcao 2"""
+
+def tabuleiro(n):
+    """Inicializa um tabuleiro"""
+    
+    #inicialização do tabuleiro (matriz) nxn
+    tabuleiro=[[0 for i in range(n)] for j in range(n)]
+    
+    #entrada de dados pelo usuário (não há validação de inputs)
+    for i in range(n):
+        row = input("")
+        for j in range(n):
+            tabuleiro[i][j] = int(row.split()[j])
+
+    for i in range(n):
+        for j in range(n):
+            print(tabuleiro[i][j], end=" ")
+        print("")
+    
+    return tabuleiro
 
 def ladrilho(pmax):
     """Funcao recursiva que verifica se existe
@@ -16,27 +63,5 @@ def ladrilho(pmax):
 
     return existe
 
-def main():
-    opcao = int(input("Opcao de jogo: "))
-    n = int(input("Tamanho do tabuleiro: "))
-
-    print("Tabuleiro inicial: ")
-
-    tabuleiro=[]
-    for i in range(n):
-            tabuleiro[i][] = list(map(int, input().split()))
-    
-    print("\n")
-    for i in range(n):
-        for j in range(n):
-            print(tabuleiro[i][j], " ")
-    print("\n")
-
-    if (opcao == 0):
-        opcao0()
-    elif (opcao == 1):
-        opcao1()
-    elif (opcao == 2):
-        opcao2()
-
+#executa o programa em si
 main()
