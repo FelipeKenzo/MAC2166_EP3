041b3eb099d6a30b350e296602e1dad8f06ed218
FelipeKenzo <felipe.kenzo@live.com>
2019-06-07 23:14:03 -0300

    Opção 1

--- mac2166_ep3.py
+++ mac2166_ep3.py
@@ -2,6 +2,7 @@ def main():
     opcao = int(input("Opcao de jogo: "))
     n = int(input("Tamanho do tabuleiro: "))
 
+    #Roda a opção 0 <= n <= 2.
     if (opcao == 0):
         opcao0(n)
     elif (opcao == 1):
@@ -10,14 +11,19 @@ def main():
         opcao2(n)
 
 def opcao0(n):
-    """Opcao 0"""
+    """Opção 0."""
     
+    #cria o tabuleiro inicial.
     print("Tabuleiro inicial:", end = " ")
-    tabInicial = tabuleiro(n)
+    tabInicial = montarTabuleiro(n)
+    #imprimir(tabInicial)
 
+    #cria o tabuleiro final.
     print("Tabuleiro final:", end = " ")
-    tabFinal = tabuleiro(n)
+    tabFinal = montarTabuleiro(n)
+    #imprimir(tabFinal)
 
+    #compara os tabuleiros valor a valor.
     iguais = True
     for i in range(n):
         for j in range(n):
@@ -30,34 +36,125 @@ def opcao0(n):
         print("NAO")
 
 def opcao1(n):
-    """Opcao 1"""
+    """Opção 1."""
+
+    #cria o tabuleiro inicial.
+    print("Tabuleiro inicial:", end = " ")
+    tabuleiro = montarTabuleiro(n)
+    #imprimir(tab)
     
+    #localiza a posição do 0 no tabuleiro. Assume que só há um 0.
+    for i in range(n):
+        for j in range(n):
+            if (tabuleiro[i][j] == 0):
+                zero = [i, j] #salva as coordenadas do zero.
+    
+    #print("zero: [",zero[0],"][",zero[1],"]")
+
+    m = int(input("Insira o numero de movimentos: "))
+
+    #inicialização da lista de movimentos
+    movimentos = [None for i in range (m)]
+
+    #entrada de dados pelo usuário. Cuidado: se não há o número exato de caracteres, dá pau.
+    movimentos = input("Movimentos: ").split()
+
+    # Apenas para debug. Deve ser retirado.
+    #print("")
+    #for i in range(m):
+    #    print(movimentos[i], end = " ")
+    #print("")
+
+    for i in range(m):
+        mover(movimentos[i], tabuleiro, zero, n)
+    
+    imprimir(tabuleiro)
+
+
 def opcao2(n):
     """Opcao 2"""
 
-def tabuleiro(n):
-    """Inicializa um tabuleiro"""
+def montarTabuleiro(n):
+    """Monta um tabuleiro."""
     
     #inicialização do tabuleiro (matriz) nxn
-    tabuleiro=[[0 for i in range(n)] for j in range(n)]
+    tabuleiro = [[0 for i in range(n)] for j in range(n)]
     
     #entrada de dados pelo usuário (não há validação de inputs)
     for i in range(n):
-        row = input("")
+        linha = input("")
         for j in range(n):
-            tabuleiro[i][j] = int(row.split()[j])
+            tabuleiro[i][j] = int(linha.split()[j])
 
-    for i in range(n):
-        for j in range(n):
-            print(tabuleiro[i][j], end=" ")
-        print("")
+    # Apenas para fins de debug. Deve ser retirado.
+    # imrimir(tabuleiro)
     
     return tabuleiro
 
+def mover(movimento, tabuleiro, posicao, n):
+    """Move um elemento qualquer de um tabuleiro"""
+    # Argumentos são passados por referência :D
+    
+    # Lembrando que:
+    #   0 1 2
+    # 0 a b c
+    # 1 d e f
+    # 2 g h i
+
+    # Apenas para facilitar a leitura
+    i = posicao[0] # linha
+    j = posicao[1] # coluna
+
+    if (movimento ==  'c'):
+        if (i <= 0): #limite superior
+            print("NAO")
+            return
+        aux = tabuleiro[i-1][j]             # copia elemento acima.
+        tabuleiro[i-1][j] = tabuleiro[i][j] # sobrescreve o elemento acima.
+        tabuleiro[i][j] = aux               # reinsere o elemento.
+        posicao[0] = posicao[0] - 1
+
+    elif (movimento ==  'b'):
+        if (i >= n - 1): # limite inferior
+            print("NAO")
+            return
+        aux = tabuleiro[i+1][j]             # copia elemento abaixo.
+        tabuleiro[i+1][j] = tabuleiro[i][j] # sobrescreve o elemento abaixo.
+        tabuleiro[i][j] = aux               # reinsere o elemento.
+        posicao[0] = posicao[0] + 1
+
+    elif (movimento ==  'd'):
+        if (j >= n - 1): #limite à direita
+            print("NAO")
+            return
+        aux = tabuleiro[i][j+1]             # copia elemento à direita.
+        tabuleiro[i][j+1] = tabuleiro[i][j] # sobrescreve o elemento à direita.
+        tabuleiro[i][j] = aux               # reinsere o elemento.
+        posicao[1] = posicao[1] + 1
+
+    elif (movimento ==  'e'):
+        if (j <= 0): #limite superior
+            print("NAO")
+            return
+        aux = tabuleiro[i][j-1]             # copia elemento à esquerda.
+        tabuleiro[i][j-1] = tabuleiro[i][j] # sobrescreve o elemento à esquerda.
+        tabuleiro[i][j] = aux                 # reinsere o elemento.
+        posicao[1] = posicao[1] - 1
+    
+    #imprimir(tabuleiro)
+
+def imprimir(tabuleiro):
+    """Imprime o tabuleiro na tela."""
+    print("")
+    for i in range(len(tabuleiro)):
+        for j in range(len(tabuleiro[i])):
+            print(tabuleiro[i][j], end=" ")
+        print("")
+    print("")
+
 def ladrilho(pmax):
-    """Funcao recursiva que verifica se existe
-       uma sequencia de pmax movimentos da
-       configuracao atual ate final """
+    """Funcao recursiva que verifica se existe uma sequência de pmax
+       movimentos da configuracao atual atá final """
 
     existe = False
 
